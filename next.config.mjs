5545663b3cedab8c16bbb7dc123e4cebbb58867b
AnsariBushra <bushraansari.0517@gmail.com>
2025-05-08 19:45:58 +0530

    modified files

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
+    domains: [
       "cdn.pixabay.com",
       "media.istockphoto.com",
       "www.istockphoto.com",
